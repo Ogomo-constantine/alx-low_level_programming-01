7018a8d873456ef646430ee3e3dab49b35ce092e
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-11 07:00:34 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers in base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
