bf62b69cde0b89f82415122d792ac3e1573cfc8a
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-14 00:58:36 -0800

    10x-alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints alphabets 10 times in lowercase
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char ch;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		       _putchar(ch);
+		_putchar('\n');
+	}
+}
