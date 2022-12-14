c424caf40af8ac46e09696febee9c837256fa6a2
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-14 00:05:28 -0800

    print-alphabet-void

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char  ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
