fffdb1124f37f22f1ccf122d0b5352e817615c72
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-14 14:46:02 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all multiples nos. of 3 or 5
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, x = 0;
+
+	while (i = 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
