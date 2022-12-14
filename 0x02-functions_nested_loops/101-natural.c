d6865299513307d566059fafff675675eac08b21
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-14 14:49:58 -0800

    natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i, x = 0;
 
-	while (i = 1024)
+	while (i < 1024)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			z += i;
+			x += i;
 		}
 		i++;
 	}
-	printf("%d\n", z);
+	printf("%d\n", x);
 	return (0);
 }
