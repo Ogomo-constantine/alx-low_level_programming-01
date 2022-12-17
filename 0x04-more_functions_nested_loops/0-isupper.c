4c4a0f043e4ac4578dc914688c70da70cf093926
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-17 02:18:22 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isuper - checks the uppercase character
- * @x: the number to be checked
+ * @c: the number to be checked
  * Return: 1 for uppercase character, 0 otherwise
  */
 
-int _isupper(int x);
+int _isupper(int c)
 
 {
 
-	if (c x= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
