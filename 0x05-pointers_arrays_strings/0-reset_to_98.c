2ec8d2154a20198f66a6f7713f54f740f5029180
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-19 03:47:50 -0800

    98-battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a poiter as int to a parameter
+ * and add the value that it points to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
