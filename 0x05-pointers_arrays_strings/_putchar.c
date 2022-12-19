bfccda1102e41949ad7ae6c54bea994dffbdbfbf
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-19 04:29:08 -0800

    _putchar

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes character to stdout
+ * @c: character to be printed
+ * Return: on success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
