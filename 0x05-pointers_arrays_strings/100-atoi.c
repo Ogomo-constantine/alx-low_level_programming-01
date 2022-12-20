f341dc7ee7aa019cdfff04374b4f564b3e4a4457
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-20 08:29:32 -0800

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 	int sign;
 	unsigned int num;
 	char *temp;
-
+	
 	temp = s;
 	num = 0;
 	sign = 1;
@@ -26,7 +26,7 @@ int _atoi(char *s)
 			num = num * 10 + (*temp - '0');
 			temp++;
 			}
-		while (*temp >= '0' & *temp <= '9');
+		while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
