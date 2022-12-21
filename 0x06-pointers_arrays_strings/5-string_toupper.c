064508ebe3c604961705a05eed8dd826df5a2ed9
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-21 12:47:52 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes al lowercases letters of a string to uppercase
+ * @str: parameter
+ * Return: a character
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i++])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
