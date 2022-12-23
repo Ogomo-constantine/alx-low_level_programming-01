c40e591b532c06d73be1c68d8d45a876d737316c
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-23 14:01:46 -0800

    i am kindof paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,15 +4,16 @@
  * reverse_array - function that reverses the content of an array of integers
  * @a: array
  * @n: no. of element
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
 	int temp, i;
 
-	for (i = n - 1; i > n / 2; i--)
+	for (i = 0; i < n--; i++)
 	{
-		tmp = a[n - 1 - i];
-		a[n - 1 - i] = a[i];
-		a[i] = tmp;
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
 	}
 }
