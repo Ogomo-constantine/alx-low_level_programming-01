05633b3deb96cabcb815c1a8d1b7a127f0113a3d
Ogomo-constantine <ogomoshiro@gmail.com>
2022-12-23 11:59:19 -0800

    A dream doesn't become reality thru magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 2024;
+	p = &n;
+
+/*
+ * write your line of code here
+ * Remember: - you are  nit allowed to use a
+ * - you are not supposed to modify p
+ * - you can only use one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
+
