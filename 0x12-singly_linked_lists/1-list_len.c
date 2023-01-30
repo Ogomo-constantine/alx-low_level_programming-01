7eabd4f85b1db49e04127afb7c4997fc6e3af166
Ogomo-constantine <ogomoshiro@gmail.com>
2023-01-30 12:54:27 -0800

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * list_len - returns the no. of elements in lins_t list
+ * @h: pointer of the list_t list
+ * Return: no. of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
+
